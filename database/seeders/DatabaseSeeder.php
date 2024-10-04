8ab54e02b9f00caeb9e5b5fd5159aee797f270d6
Phillip César <phillipcesar@MacBook-Air-de-Phillip.local>
2024-10-03 23:57:44 -0300

    Adicionando Permissões/Papéis aos usuários

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,8 @@ namespace Database\Seeders;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
+
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,7 +15,15 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+
+        // Criar papéis
+        $admin = Role::create(['name' => 'administrator']);
+        $client = Role::create(['name' => 'client']);
+        $guest = Role::create(['name' => 'guest']);
         // User::factory(10)->create();
+
+        $admin->givePermissionTo(['view filament', 'create users', 'manage users', 'other admin permissions']);
+        $client->givePermissionTo(['view filament', 'create users']);
 
         User::factory()->create([
             'name' => 'Test User',
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
+use Filament\Facades\Filament;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Auth\LoginController;
 
 Route::get('/', function () {
     return view('welcome');
